332652a6abc6a3eae25f943545d90a4566deefd9
Caleb Mcolin <Caleb.mcolin@gmail.com>
2021-06-01 14:20:56 -0700

    Convert to ES6 module/Formatting.

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
+import { launch } from 'puppeteer';
+import { appendFileSync } from 'fs';
 
-const array = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const array = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 (async () => {
     // Creating the brower
-    const browser = await puppeteer.launch();
+    const browser = await launch();
     const page = await browser.newPage();
     await page.goto('https://www.gamertagavailability.com/');
-  
+
     var counter = 0;
     // Loop that cycles through every single letter compination of every 4 letter word (26^4 = 456,976 options)
-    for(var i = 0; i < 26; i++){
-        for(var j = 0; j < 26; j++){
-            for(var k = 0; k < 26; k++){
-                for(var l = 0; l < 26; l++){
+    for (var i = 0; i < 26; i++) {
+        for (var j = 0; j < 26; j++) {
+            for (var k = 0; k < 26; k++) {
+                for (var l = 0; l < 26; l++) {
                     counter++;
                     // Entering a text into the textbox on the web page
-                    await page.type('#mytag', array[i]+array[j]+array[k]+array[l]);
+                    await page.type('#mytag', array[i] + array[j] + array[k] + array[l]);
 
                     // Clicking the button that will lead to the next page
                     await Promise.all([
@@ -27,18 +27,18 @@ const array = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','
 
                     // Checking certain IDs are on the new page.
                     // This says if the name is available or not.
-                    const [yes] = await page.$x('//*[@id="yres"]');
-                    if(yes == undefined){
+                    const yes = await page.$x('//*[@id="yres"]');
+                    if (yes == undefined) {
                         // Nothing
-                    }else{
-                        const [no] = await page.$x('//*[@id="nres"]'); 
-                        if(no === undefined){
-                            fs.appendFileSync('PossibleNames.txt', array[i]+array[j]+array[k]+array[l] + "\n");
-                        }else{
+                    } else {
+                        const no = await page.$x('//*[@id="nres"]');
+                        if (no === undefined) {
+                            appendFileSync('PossibleNames.txt', array[i] + array[j] + array[k] + array[l] + "\n");
+                        } else {
                             console.log('Error');
                         }
-                    }
-                    
+                    };
+
                     //returning to the main page
                     await Promise.all([
                         page.waitForNavigation(),
@@ -46,8 +46,8 @@ const array = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','
                     ]);
                 }
             }
-        }  
+        }
     }
-    fs.appendFilesSync('Tried: ' + counter + 'Words');
+    appendFilesSync('Tried: ' + counter + 'Words');
     await browser.close();
 })();
--- app2.js
+++ app2.js
@@ -1,21 +1,21 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
+import { launch } from 'puppeteer';
+import { appendFileSync } from 'fs';
 
-const array = ['','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const array = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 (async () => {
     // Creating the brower
-    const browser = await puppeteer.launch();
+    const browser = await launch();
     const page = await browser.newPage();
     await page.goto('https://www.gamertagavailability.com/');
-  
+
     var counter = 0;
     // Loop that cycles through every single letter combination of 4-5 letter words that end with "tek"
-    for(var i = 0; i < 27; i++){
-        for(var j = 1; j < 27; j++){// doesn't include the empty space (array[0]) in the second spot
-            
+    for (var i = 0; i < 27; i++) {
+        for (var j = 1; j < 27; j++) {// doesn't include the empty space (array[0]) in the second spot
+
             // Entering a text into the textbox on the web page
-            await page.type('#mytag', array[i]+array[j]) + 'xxx';
+            await page.type('#mytag', array[i] + array[j]) + 'xxx';
 
             // Clicking the button that will lead to the next page
             await Promise.all([
@@ -25,22 +25,22 @@ const array = ['','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p
 
             // Checking certain IDs are on the new page.
             // This says if the name is available or not.
-            const [yes] = await page.$x('//*[@id="yres"]');
-            if(yes == undefined){
+            const yes = await page.$x('//*[@id="yres"]');
+            if (yes == undefined) {
                 // Nothing
-            }else{
-                const [no] = await page.$x('//*[@id="nres"]'); 
-                if(no === undefined){
-                    fs.appendFileSync('NamesEndWithXxx.txt', + array[i]+array[j] + 'xxx');
-                }else{
+            } else {
+                const no = await page.$x('//*[@id="nres"]');
+                if (no === undefined) {
+                    appendFileSync('NamesEndWithXxx.txt', + array[i] + array[j] + 'xxx');
+                } else {
                     console.log('Error');
                 }
             }
-            
+
             // Tracking the progress
             counter++;
-            if(array[j] == 'z'){
-                console.log('Starting: '+ array[i+1] + 's');
+            if (array[j] == 'z') {
+                console.log('Starting: ' + array[i + 1] + 's');
             }
 
             //returning to the main page
@@ -48,8 +48,8 @@ const array = ['','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p
                 page.waitForNavigation(),
                 page.goto('https://www.gamertagavailability.com/')
             ]);
-        }  
+        }
     }
-    fs.appendFileSync('NamesBegin-lay.txt', 'Tried: ' + counter + 'Words');
+    appendFileSync('NamesBegin-lay.txt', 'Tried: ' + counter + 'Words');
     await browser.close();
 })();
