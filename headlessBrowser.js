7c9f27293d334e51a66cb1027079f54daa7e9f85
Caleb Mcolin <Caleb.mcolin@gmail.com>
2021-06-23 20:15:11 -0700

    Minor fixes

--- headlessBrowser.js
+++ headlessBrowser.js
@@ -1,6 +1,6 @@
 import puppeteer from 'puppeteer';
 import { appendFileSync } from 'fs';
-import { menu } from './main';
+import { menu } from './main.js';
 
 const letterArray = [' ', '', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
@@ -45,7 +45,7 @@ async function findAvalibleNames(nameSet, input) {
   const page = await browser.newPage();
   await page.goto('https://www.gamertagavailability.com/');
 
-  appendFileSync(`AvailableNames-${input}.txt`, "These are a list of names that seem to be available...");
+  appendFileSync(`AvailableNames-${input}.txt`, "These are a list of names that seem to be available... \n");
 
   let counter = 0;
   for (let name of nameSet) {
